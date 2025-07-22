08a23a6cdeb258839c2cd2e6f9114db3728ae752
tpillow01 <thomasph9@gmail.com>
2025-07-22 15:54:39 -0400

    Add PWA support with manifest, service worker, and icons

---
+++ static/service-worker.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  console.log('Service Worker installed');
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
